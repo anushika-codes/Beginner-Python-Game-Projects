349d0ce4136f199342e0fa354399078e2689054a
Anushika <anushika10073@gmail.com>
2025-07-29 09:21:03 +0530

    Completed

--- Game 03 - Number Guessing Game/number_guessing_game.py
+++ Game 03 - Number Guessing Game/number_guessing_game.py
@@ -1,1 +1,22 @@
+#Number Guessing Game
+'''
+Computer will choose a number from 1 to 100
+You have to guess it
+You keep guessing until you get it right.
+'''
+
+import random
+
+number = random.randint(1, 100)
+guess = None
+
+while guess != number:
+    guess = int(input("Guess the number between 1 and 100: "))
+
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    else:
+        print("Congratulations! You guessed it right.")
 
