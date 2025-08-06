fcb1eba3e108e6af68403fb083a288fea2158eae
Anushika <anushika10073@gmail.com>
2025-08-06 18:45:34 +0530

    Initial color guessing game script

--- Game 19 - Color Guessing Game/guess_color.py
+++ Game 19 - Color Guessing Game/guess_color.py
@@ -1,1 +1,29 @@
+import random
+
+# List of possible colors
+colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'black', 'white']
+
+# Randomly select a color
+secret_color = random.choice(colors)
+
+print("Welcome to the Color Guessing Game!")
+print("Guess the color I'm thinking of...")
+print("Hint: Choose from these colors:", ", ".join(colors))
+print("You have 5 attempts. Good luck!\n")
+
+# Number of allowed guesses
+attempts = 5
+
+while attempts > 0:
+    guess = input("Enter your guess: ").lower()
+
+    if guess == secret_color:
+        print("Congratulations! You guessed it right.")
+        break
+    else:
+        attempts -= 1
+        print(f"Wrong guess. Attempts left: {attempts}")
+
+if attempts == 0:
+    print(f"\n Sorry, you've run out of attempts. The correct color was '{secret_color}'.")
 
